79653d061b03577f0fa58371214b307bcf9bedcd
NH Evan <nhevan@gmail.com>
2018-05-30 17:20:12 +0600

    Rover is ready to take the body to a test run.

--- nhrover/Models/Rover.php
+++ nhrover/Models/Rover.php
@@ -52,13 +52,52 @@ class Rover implements RoverInterface
         $this->logger->info("Powering up rover ...");
     }
 
+    /**
+     * The rover moves forward for x seconds and stops
+     * @param int $steps
+     */
     public function stepAhead($steps = 1)
     {
-
+        $this->logger->info("Initiating Stepping ahead ...");
+        $this->body->moveForward();
+        sleep(1);
+        $this->body->stop();
     }
 
+    /**
+     * @param int $steps
+     */
     public function stepBack($steps = 1)
     {
-        // TODO: Implement stepBack() method.
+        $this->logger->info("Initiating Stepping back ...");
+        $this->body->moveBackward();
+        sleep(1);
+        $this->body->stop();
+    }
+
+    /**
+     * @param int $steps
+     */
+    public function turnLeft($steps = 1)
+    {
+        $this->logger->info("Initiating turn left command ...");
+        $this->body->turnLeft();
+        usleep(500);
+        $this->body->stop();
+    }
+
+    public function turnRight($steps = 1){
+        $this->logger->info("Initiating turn right command ...");
+        $this->body->turnRight();
+        usleep(500);
+        $this->body->stop();
+    }
+
+    public function testDrive(){
+        $this->logger->info("Initiating a test drive ...");
+        $this->stepAhead();
+        $this->stepAhead();
+        $this->stepBack();
+        $this->turnRight()
     }
 }
